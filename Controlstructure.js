8d99fdb9c530492a6f6aede50fc7153cad160edd
Jitendra <jitendrasinh1111@gmail.com>
2025-02-07 14:50:21 +0530

    First Program

---
+++ Controlstructure.js
@@ -0,0 +1,45 @@
+
+        //If...else statement
+
+let a = 10
+let b = 30
+
+if(a>b)
+    console.log("a is greater than b")
+else
+    console.log("b is greater than a")
+
+
+        // Print 1 to 10 numbers
+
+let i = 1;
+while(i<=10)
+{
+    console.log(i)
+    i++
+}
+        // Print 10 to 1 numbers
+    
+let j = 10;
+while(j>=1)
+{
+    console.log(j)
+    j--
+}
+
+        // Do...while loop
+let k = 1;
+do
+{
+    console.log(k);
+    k++;
+}
+while(k<=10)
+
+
+        // For loop    
+for(i=1;i<=10;i++)
+    console.log(i)
+
+
+    
---
+++ Helloworld.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+var a = 10;
+const b = 15;
+var c = 12.5;
+var str = "Qa";
+let e = 15;
+var a = 22;
+
+console.log("A = " +a)
+console.log("B = "+b)
+console.log("C = "+c)
+console.log("D = "+str)
+console.log("E = "+e)
+console.log("a = "+a)
+
+
+
+
+
