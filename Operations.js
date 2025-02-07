31214105927d4e10dd4d2c2bbcfb8b163b5174b7
Jitendra <jitendrasinh1111@gmail.com>
2025-02-07 18:06:33 +0530

    Third

---
+++ Operations.js
@@ -0,0 +1,22 @@
+function addition(a,b)
+{
+    let c=a+b;
+    console.log("Addition is = "+c)
+}
+
+addition(5,7)
+
+
+let add = function(a,b)    // F
+{
+    let d=a+b;
+    console.log("Addtion is = "+d)
+}
+add(15,20)
+
+function substraction(a,b)
+{
+    let c=a-b;
+    console.log("Substraction is = "+c)
+}
+substraction(15,10)
---
+++ Operations2.js
@@ -0,0 +1,22 @@
+let myvalue  = function(a,b,c)
+{
+    d=a+b+c;
+    return d;
+}
+let val = myvalue (5,10,15)
+
+let bool  = function(a)
+{
+    let jit = Boolean
+    if (a > 10){
+jit = true
+    }else{
+jit = false
+    }
+    return jit
+}
+let value = bool(15)
+
+console.log("Addition is = "+val)
+console.log("num is"+value)
+
